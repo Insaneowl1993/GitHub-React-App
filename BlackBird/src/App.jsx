16a67d3377c66e212d16d881a33113f6b0698720
Dale Lambert <inaneowl@gmail.com>
2024-04-09 08:27:46 +1200

    Updated Logo

--- BlackBird/src/App.jsx
+++ BlackBird/src/App.jsx
@@ -9,25 +9,10 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href="https://www.propelleraero.com/dirtmate/" target="_blank">
+          <img src="https://www.propelleraero.com/wp-content/uploads/2023/05/logo.svg" className="logo" alt="DirtMate" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
